821d409cbd130619aa7cf63d2a21acccce0af0cb
YahyaMohand <96312354+YahyaMohand@users.noreply.github.com>
2023-10-05 15:55:19 +0300

    update the code

--- notebook/bank_pre.py
+++ notebook/bank_pre.py
@@ -1,17 +1,12 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[25]:
-
-
+import warnings
+warnings.filterwarnings('ignore')
 import numpy as np
 import pandas as pd
-
-
-
 from sklearn.model_selection import train_test_split
-from sklearn.metrics import (accuracy_score, f1_score,average_precision_score, confusion_matrix,
-                             average_precision_score, precision_score, recall_score, roc_auc_score, )
+from sklearn.metrics import (
+    accuracy_score, f1_score, average_precision_score, confusion_matrix,
+    precision_score, recall_score, roc_auc_score,
+)
 from sklearn.model_selection import GridSearchCV
 from sklearn.naive_bayes import GaussianNB
 from sklearn.linear_model import LogisticRegression
@@ -19,123 +14,76 @@ from sklearn import tree
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.preprocessing import LabelEncoder, MinMaxScaler, StandardScaler
-
-
 from xgboost import XGBClassifier, plot_importance
 from imblearn.over_sampling import SMOTE
 from urllib.parse import urlparse
-
 import mlflow
 import mlflow.sklearn
 
 
-# In[3]:
+remote_server_uri = "https://dagshub.com/YahyaMohand/bank_customers.mlflow"
+mlflow.set_tracking_uri(remote_server_uri)
 
-
+# Load the dataset
 dataset = pd.read_csv("../data/Churn Modeling.csv")
 
-
-# In[4]:
-
-
-dataset.head()
-
-
-# In[5]:
-
-
-dataset.describe()
-
-
-# In[6]:
-
-
-dataset.info()
-
-
-# In[7]:
-
-
+# Drop unnecessary columns
 dataset.drop(["RowNumber", "CustomerId", "Surname"], axis=1, inplace=True)
 
-
-# In[8
-# In[10]:
-
-
+# Encode categorical variables
 encoder = LabelEncoder()
 dataset["Geography"] = encoder.fit_transform(dataset["Geography"])
 dataset["Gender"] = encoder.fit_transform(dataset["Gender"])
 
-
-# In[11]:
-
-
-dataset["Age"].value_counts().plot.bar(figsize=(20,6))
-
-
-
+# Scale features
 scaler = MinMaxScaler()
-df_scaled = pd.DataFrame(data = dataset)
+df_scaled = pd.DataFrame(data=dataset)
 df_scaled[:] = scaler.fit_transform(dataset[:])
 
-
-# In[20]:
-
-
-df_scaled.head()
-
-
-# In[21]:
-
-
+# Define features and target variable
 X = df_scaled.drop("Exited", axis=1)
 y = df_scaled["Exited"]
 
+# Split the dataset into training and testing sets
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0, stratify=y)
 
-# In[22]:
+parameters = {
+    'criterion': ['gini', 'entropy'],
+    'max_depth': [2 * n for n in range(1, 10)],
+    'max_features': ['auto', 'sqrt'],
+    'min_samples_leaf': [1, 2, 4],
+    'min_samples_split': [2, 5, 10]
+}
 
+clf = RandomForestClassifier(criterion='gini', max_depth=18, max_features='sqrt', min_samples_leaf=1, min_samples_split=2, random_state=200)
+clf_cv = GridSearchCV(clf, parameters, cv=10)
+clf_cv.fit(X_train, y_train)
 
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0,stratify=y)
-
-
-# In[26]:
-
-
+# Start an MLflow run
 with mlflow.start_run():
-    clf = GaussianNB()
+
+    # Log hyperparameters from GridSearchCV
+    mlflow.log_params(clf_cv.best_params_)
+
+    # Initialize and train the RandomForestClassifier with tuned hyperparameters
+    clf = RandomForestClassifier(**clf_cv.best_params_, random_state=200)
     clf.fit(X_train, y_train)
     y_pred = clf.predict(X_test)
-    print("Accuracy:", accuracy_score(y_test, y_pred))
-    print("Precision:", precision_score(y_test, y_pred))
-    print("Recall:", recall_score(y_test, y_pred))
-    print("F1:", f1_score(y_test, y_pred))
-    print("Area under precision (AUC) Recall:", average_precision_score(y_test, y_pred))
-    mlflow.log_param("alpha", accuracy_score(y_test, y_pred))
-    mlflow.log_param("l1_ratio", precision_score(y_test, y_pred))
-    mlflow.log_metric("rmse", recall_score(y_test, y_pred))
-    mlflow.log_metric("r2", f1_score(y_test, y_pred))
 
-    remote_server_uri="https://dagshub.com/YahyaMohand/bank_customers.mlflow"
+    # Evaluate the model and log metrics
+    accuracy = accuracy_score(y_test, y_pred)
+    precision = precision_score(y_test, y_pred)
+    recall = recall_score(y_test, y_pred)
+    f1 = f1_score(y_test, y_pred)
+    average_precision = average_precision_score(y_test, y_pred)
+
+    # Log metrics
+    mlflow.log_metric("accuracy", accuracy)
+    mlflow.log_metric("precision", precision)
+    mlflow.log_metric("recall", recall)
+    mlflow.log_metric("f1_score", f1)
+    mlflow.log_metric("average_precision", average_precision)
+
+    # Set the remote MLflow tracking server URI
+    remote_server_uri = "https://dagshub.com/YahyaMohand/bank_customers.mlflow"
     mlflow.set_tracking_uri(remote_server_uri)
-
-    tracking_url_type_store = urlparse(mlflow.get_tracking_uri()).scheme
-
-        # Model registry does not work with file store
-    if tracking_url_type_store != "file":
-            # Register the model
-            # There are other ways to use the Model Registry, which depends on the use case,
-            # please refer to the doc for more information:
-            # https://mlflow.org/docs/latest/model-registry.html#api-workflow
-        mlflow.sklearn.log_model(
-            clf, "model", registered_model_name="clf"
-        )
-    else:
-        mlflow.sklearn.log_model(clf, "model")
-
-
-# In[ ]:
-
-
-
-
